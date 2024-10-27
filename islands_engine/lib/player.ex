6f40ebbd8f53cd53fd260a6eea96f92262faf900
Adi-Sin <adiaggarwal26@gmail.com>
2024-10-27 09:20:55 +0000

    feat: add player module

---
+++ islands_engine/lib/player.ex
@@ -0,0 +1,39 @@
+defmodule IslandsEngine.Player do
+  alias IslandsEngine.{Board, IslandSet, Player}
+
+  # A player has a name, a board, and an island set
+  defstruct name: :none, board: :none, island_set: :none
+
+  def start_link(name \\ :none) do
+    {:ok, board} = Board.start_link
+    {:ok, island_set} = IslandSet.start_link
+    Agent.start_link(fn -> %Player{board: board, island_set: island_set, name: name} end)
+  end
+
+  def set_name(player, name) do
+    Agent.update(player, fn state -> Map.put(state, :name, name) end)
+  end
+
+  def get_board(player) do
+    Agent.get(player, fn state -> state.board end)
+  end
+
+  def get_island_set(player) do
+    Agent.get(player, fn state -> state.island_set end)
+  end
+
+  def to_string(player) do
+    "%Player{" <> string_body(player) <> "}"
+  end
+
+  def string_body(player) do
+    state = Agent.get(player, &(&1))
+    ":name => " <> name_to_string(state.name) <> ",\n" <>
+    ":island_set => " <> IslandSet.to_string(state.island_set) <> ",\n" <>
+    ":board => " <> Board.to_string(state.board)
+  end
+
+  defp name_to_string(:none), do: ":none"
+  defp name_to_string(name), do: ~s("#{name}")
+
+end
